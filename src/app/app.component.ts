60ac045e1c734bcd1963c0df2b1bdff550e21a49
Sakib Hasan <sakib8899@gmail.com>
2021-10-09 21:12:54 +0200

    refactor: updated box shadow; unsubscribe added for observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { PostService } from './services/post.service';
 import { Post } from './models/post';
+import { Subject } from 'rxjs';
+import { takeUntil, tap } from 'rxjs/operators';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
     posts: Post[] = [];
-    constructor(private postService: PostService) {
-        this.postService.GetPosts().subscribe((posts) => {
-            this.posts = posts.map((p) => {
-                p.body = p.body.length > 180 ? p.body.slice(0, 180) + '...' : p.body;
-                return p;
+    private ngUnsubscribe = new Subject();
+
+    constructor(private postService: PostService) {}
+
+    ngOnInit() {
+        this.postService
+            .GetPosts()
+            .pipe(
+                tap((posts) =>
+                    posts.map((p) => ({
+                        ...p,
+                        body: p.body.length > 180 ? `${p.body.slice(0, 180)}...` : p.body,
+                    })),
+                ),
+                takeUntil(this.ngUnsubscribe),
+            )
+            .subscribe((posts) => {
+                this.posts = posts;
             });
-        });
+    }
+
+    ngOnDestroy() {
+        this.ngUnsubscribe.next();
+        this.ngUnsubscribe.complete();
     }
 }
